834fff0a5b6e30b4f86bd512890d69f2b857edb2
Emmanuel George <Emmanuelgeorge1000@gmail.com>
2022-08-26 15:53:01 +0530

    Initial Commit

---
+++ EmmanuelCard/EmmanuelCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmmanuelCardApp.swift
+//  EmmanuelCard
+//
+//  Created by Emmanuel George on 26/08/22.
+//
+
+import SwiftUI
+
+@main
+struct EmmanuelCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
